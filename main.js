c3a9b52294f8a090f609ebf2bc81b8fcfd84bae0
Grady Morgan <gradymorgan@gmail.com>
2019-03-23 10:45:25 -0700

    updates for dockerization

--- main.js
+++ main.js
@@ -10,153 +10,171 @@ var moment = require('moment');
 var aqi = require('aqi-us');
 
 function initRestAPI() {
-	var app = express();
+    var app = express();
 
-	app.use(bodyParser.json());
-	app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
 
-	var server = app.listen(3000, function () {
-	    console.log("app running on port.", server.address().port);
-	});
+    var server = app.listen(3000, function () {
+        console.log("rest api running on port.", server.address().port);
+    });
 
-	return app;
+    app.use(function(req, res, next) {
+        console.log('requested ' + req.originalUrl);
+        next();
+    });
+
+    return app;
 }
 
 function initInflux() {
-	var influx = new Influx.InfluxDB({
-		host: '192.168.7.101',
-		port: 8086,
-	    database: 'weather'
-	});
+    var influx = new Influx.InfluxDB({
+        host: 'influxdb',
+        port: 8086,
+        database: 'weather'
+    });
 
-	return influx;
+    return influx;
 }
 
 function main() {
-	var influx = initInflux();
-	var rest_server = initRestAPI();
+    var influx = initInflux();
+    var rest_server = initRestAPI();
 
 
-	var messagePump = new EventEmitter();
-	messagePump.on('sample', function(data) {
-		influx.writePoints(data.map(function(d) {
-				console.info(d);
-				return {
-    			measurement: d.measurement,
-    			tags: { source: d.source },
-    			fields: { units: d.units, value: d.value },
-    			timestamp: d.time
-	    		};
-  			}));
-	});
+    var messagePump = new EventEmitter();
+    messagePump.on('sample', function(data) {
+        influx.writePoints(data.map( (d) => {
+            return {
+                measurement: d.measurement,
+                tags: { source: d.source },
+                fields: { units: d.units, value: d.value },
+                timestamp: d.time
+            }
+        }));
+    });
 
-	listenToWeatherflow(messagePump, null, null);
-	listenToPurple(messagePump, rest_server, null);
+    listenToWeatherflow(messagePump, null, null);
+    listenToPurple(messagePump, rest_server, null);
 }
 main();
 
 function listenToWeatherflow(pump, server, options) {
-	var PORT = 50222;
-	var server = dgram.createSocket('udp4');
+    var PORT = 50222;
+    var server = dgram.createSocket('udp4');
 
-	server.on('listening', function () {
-	    var address = server.address();
-	    console.log('UDP Server listening on :' + PORT);
-	});
+    server.on('listening', () => {
+        console.log('UDP Server listening on :' + PORT);
+    });
 
-	server.on('message', function (message, remote) {
-	    message = JSON.parse(message);
-	    // console.log('weather flow message, type: '+message.type);
-	    var type = message.type;
-	    var data;
+    server.on('message', function (message, remote) {
+        message = JSON.parse(message);
+        // console.log('weather flow message, type: '+message.type);
+        var type = message.type;
+        var data;
 
-	    if (type == 'obs_air')
-	    	data = parseWeatherFlowAirReport(message);
-	    else if (type == 'obs_sky')
-	    	data = parseWeatherFlowSkyReport(message);
+        if (type == 'obs_air')
+            data = parseWeatherFlowAirReport(message);
+        else if (type == 'obs_sky')
+            data = parseWeatherFlowSkyReport(message);
 
-	    if (data)
-	    	pump.emit('sample', data);
+        if (data)
+            pump.emit('sample', data);
 
-	});
+    });
 
-	server.bind({ port: PORT });
+    server.bind({ port: PORT });
 }
 
 function listenToPurple(pump, restApi, options) {
-	restApi.put("/sensors/purpleair/:key", function (req, res) {
-		//key to sensor check
+    restApi.put("/sensors/purpleair/:key", (req, res) => {
+        //key to sensor check
+        console.log('purple receive');
 
-		console.info(req.body);
-		var data = parsePurpleAirReport(req.body);
+        var data = parsePurpleAirReport(req.body);
 
-		if (data) 
-			pump.emit('sample', data);
+        if (data) 
+            pump.emit('sample', data);
 
-		res.status(200).send({ok: ':)'});
-	});
+        res.status(200).send({ok: ':)'});
+    });
 }
 
+function listenToAwair(pump, restApi, options) {
+    restApi.put("/sensors/awair/:key", function (req, res) {
+        //key to sensor check
+
+        // console.info(req.body);
+        var data = [
+            {measurement:'awair', source: "awair temp", source_id: 'asdf', value: 234, units: 'nah', time: new Date()}
+        ];
+
+        if (data) 
+            pump.emit('sample', data);
+
+        res.status(200).send({ok: ':)'});
+    });
+}
 
 // https://weatherflow.github.io/SmartWeather/api/udp/v91/
 // {"serial_number":"AR-00012490","type":"obs_air","hub_sn":"HB-00004837","obs":[[1534912506,1005.70,21.38,68,0,0,3.51,1]],"firmware_revision":20}
-// 0	Time Epoch	Seconds
-// 1	Station Pressure	MB
-// 2	Air Temperature	C
-// 3	Relative Humidity	%
-// 4	Lightning Strike Count	
-// 5	Lightning Strike Avg Distance	km
-// 6	Battery	
-// 7	Report Interval	Minutes
+// 0    Time Epoch  Seconds
+// 1    Station Pressure    MB
+// 2    Air Temperature C
+// 3    Relative Humidity   %
+// 4    Lightning Strike Count  
+// 5    Lightning Strike Avg Distance   km
+// 6    Battery 
+// 7    Report Interval Minutes
 
 function parseWeatherFlowAirReport(message) {
-	var obs = message.obs[0];
-	var time_epoch = new Date(obs[0]*1000);
-	var pressure = obs[1];
-	var temp = obs[2];
-	var humidity = obs[3];
+    var obs = message.obs[0];
+    var time_epoch = new Date(obs[0]*1000);
+    var pressure = obs[1];
+    var temp = 9*obs[2]/5.0 + 32;
+    var humidity = obs[3];
 
-	return [
-		{measurement:'temp', source: "WeatherFlow Air", source_id: message.serial_number, value: temp, units: 'C', time: time_epoch},
-		{measurement:'pressure', source: "WeatherFlow Air", source_id: message.serial_number, value: pressure, units: 'MB', time: time_epoch},
-		{measurement:'humidity', source: "WeatherFlow Air", source_id: message.serial_number, value: humidity, units: '%', time: time_epoch},
-	];
+    return [
+        {measurement:'temp', source: "WeatherFlow Air", source_id: message.serial_number, value: temp, units: 'F', time: time_epoch},
+        {measurement:'pressure', source: "WeatherFlow Air", source_id: message.serial_number, value: pressure, units: 'MB', time: time_epoch},
+        {measurement:'humidity', source: "WeatherFlow Air", source_id: message.serial_number, value: humidity, units: '%', time: time_epoch},
+    ];
 }
 
 //{"serial_number":"SK-00008021","type":"obs_sky","hub_sn":"HB-00004837","obs":[[1534912516,0,0.00,0.000000,0.00,0.28,0.76,44,3.45,1,0,null,0,3]],"firmware_revision":43}
-// 0	Time Epoch	Seconds
-// 1	Illuminance	Lux
-// 2	UV	Index
-// 3	Rain Accumulated	mm
-// 4	Wind Lull (minimum 3 second sample)	m/s
-// 5	Wind Avg (average over report interval)	m/s
-// 6	Wind Gust (maximum 3 second sample)	m/s
-// 7	Wind Direction	Degrees
-// 8	Battery	Volts
-// 9	Report Interval	Minutes
-// 10	Solar Radiation	W/m^2
-// 11	Local Day Rain Accumulation	mm
-// 12	Precipitation Type	0 = none, 1 = rain, 2 = hail
-// 13	Wind Sample Interval	seconds
+// 0    Time Epoch  Seconds
+// 1    Illuminance Lux
+// 2    UV  Index
+// 3    Rain Accumulated    mm
+// 4    Wind Lull (minimum 3 second sample) m/s
+// 5    Wind Avg (average over report interval) m/s
+// 6    Wind Gust (maximum 3 second sample) m/s
+// 7    Wind Direction  Degrees
+// 8    Battery Volts
+// 9    Report Interval Minutes
+// 10   Solar Radiation W/m^2
+// 11   Local Day Rain Accumulation mm
+// 12   Precipitation Type  0 = none, 1 = rain, 2 = hail
+// 13   Wind Sample Interval    seconds
 function parseWeatherFlowSkyReport(message) {
-	var obs = message.obs[0];
-	var time_epoch = new Date(obs[0]*1000);
-	var UV = obs[2];
-	var illuminance = obs[1];
-	var rain = obs[3];
+    var obs = message.obs[0];
+    var time_epoch = new Date(obs[0]*1000);
+    var UV = obs[2];
+    var illuminance = obs[1];
+    var rain = obs[3];
 
-	var wind_speed = obs[5];
-	var wind_speed_gust = obs[6];
-	var wind_direction = obs[7];
+    var wind_speed = obs[5];
+    var wind_speed_gust = obs[6];
+    var wind_direction = obs[7];
 
-	return [
-		{measurement:'wind_direction', source: "WeatherFlow Sky", source_id: message.serial_number, value: wind_direction, units: 'deg', time: time_epoch},
-		{measurement:'wind_speed', source: "WeatherFlow Sky", source_id: message.serial_number, value: wind_speed, units: 'm/s', time: time_epoch},
-		{measurement:'wind_speed_gust', source: "WeatherFlow Sky", source_id: message.serial_number, value: wind_speed_gust, units: 'm/s', time: time_epoch},
-		{measurement:'UV', source: "WeatherFlow Sky", source_id: message.serial_number, value: UV, units: 'Index', time: time_epoch},
-		{measurement:'illuminance', source: "WeatherFlow Sky", source_id: message.serial_number, value: illuminance, units: 'Lux', time: time_epoch},
-		{measurement:'rain', source: "WeatherFlow Sky", source_id: message.serial_number, value: rain, units: 'mm', time: time_epoch}
-	];	
+    return [
+        {measurement:'wind_direction', source: "WeatherFlow Sky", source_id: message.serial_number, value: wind_direction, units: 'deg', time: time_epoch},
+        {measurement:'wind_speed', source: "WeatherFlow Sky", source_id: message.serial_number, value: wind_speed, units: 'm/s', time: time_epoch},
+        {measurement:'wind_speed_gust', source: "WeatherFlow Sky", source_id: message.serial_number, value: wind_speed_gust, units: 'm/s', time: time_epoch},
+        {measurement:'UV', source: "WeatherFlow Sky", source_id: message.serial_number, value: UV, units: 'Index', time: time_epoch},
+        {measurement:'illuminance', source: "WeatherFlow Sky", source_id: message.serial_number, value: illuminance, units: 'Lux', time: time_epoch},
+        {measurement:'rain', source: "WeatherFlow Sky", source_id: message.serial_number, value: rain, units: 'mm', time: time_epoch}
+    ];  
 }
 
 // {"serial_number":"SK-00008021","type":"rapid_wind","hub_sn":"HB-00004837","ob":[1534912518,0.54,125]}
@@ -227,23 +245,23 @@ function parseWeatherFlowTodo() {}
  */
 function parsePurpleAirReport(message) {
 
-	var time = moment(message.DateTime,'YYYY/MM/DDTHH:mm:ss').toDate();
-	var pm25AQI = aqi.pm25(message.pm2_5_cf_1);
-	var pm10AQI = aqi.pm25(message.pm10_0_cf_1);
+    var time = moment(message.DateTime,'YYYY/MM/DDTHH:mm:ss').toDate();
+    var pm25AQI = aqi.pm25(message.pm2_5_cf_1);
+    var pm10AQI = aqi.pm25(message.pm10_0_cf_1);
 
 
-	return [
-		{measurement:'PM2_5_AQI', source: "Purple Air", value: pm25AQI, units: 'index', time: time},
-		{measurement:'PM10_AQI', source: "Purple Air", value: pm10AQI, units: 'index', time: time},
-		
-		{measurement:'pm1', source: "Purple Air", value: message.pm1_0_cf_1, units: 'ug/m3', time: time},
-		{measurement:'pm2_5', source: "Purple Air", value: message.pm2_5_cf_1, units: 'ug/m3', time: time},
-		{measurement:'pm10', source: "Purple Air", value: message.pm10_0_cf_1, units: 'ug/m3', time: time},
-		
-		{measurement:'temp', source: "Purple Air", value: message.current_temp_f, units: 'F', time: time},
-		{measurement:'humidity', source: "Purple Air", value: message.current_humidity, units: '%', time: time},
-		{measurement:'pressure', source: "Purple Air", value: message.pressure, units: 'mB', time: time}
-	];	
+    return [
+        {measurement:'PM2_5_AQI', source: "Purple Air", value: pm25AQI, units: 'index', time: time},
+        {measurement:'PM10_AQI', source: "Purple Air", value: pm10AQI, units: 'index', time: time},
+        
+        {measurement:'pm1', source: "Purple Air", value: message.pm1_0_cf_1, units: 'ug/m3', time: time},
+        {measurement:'pm2_5', source: "Purple Air", value: message.pm2_5_cf_1, units: 'ug/m3', time: time},
+        {measurement:'pm10', source: "Purple Air", value: message.pm10_0_cf_1, units: 'ug/m3', time: time},
+        
+        {measurement:'temp', source: "Purple Air", value: message.current_temp_f, units: 'F', time: time},
+        {measurement:'humidity', source: "Purple Air", value: message.current_humidity, units: '%', time: time},
+        {measurement:'pressure', source: "Purple Air", value: message.pressure, units: 'mB', time: time}
+    ];  
 }
 
 
